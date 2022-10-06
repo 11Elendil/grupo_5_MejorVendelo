d0f3a69112e7641abebbd8925a4532f556401f8a
MateoF01 <mateofernandez380@gmail.com>
2022-10-06 11:38:11 -0300

    Estructura de carpetas

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const publicFolderPath = path.resolve('public')
+app.use(express.static(publicFolderPath));
+
+app.listen(3033, () => console.log("Servidor corriendo"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("views/index.html"));
+})
