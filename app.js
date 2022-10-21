cbe8ac9e4c277d23d4eab69d24e0328f2e141d1e
11Elendil <lautaro.goscilo@gmail.com>
2022-10-21 16:30:27 -0300

    Empezando a crear el login

--- app.js
+++ app.js
@@ -10,3 +10,6 @@ app.listen(3033, () => console.log("Servidor corriendo"));
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("views/index.html"));
 })
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve("views/login.html"));
+})
