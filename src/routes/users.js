fa202d879170d8c339d3bd848eed45e379d85234
Lautaro Goscilo <lautarogoscilo@Air-de-Lautaro.fibertel.com.ar>
2022-12-21 16:38:24 -0300

    agrego el campo faltante a rutas login

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,6 +12,7 @@ const {validationResult} = require('express-validator');
 const usersController = require('../controllers/usersController');
 const validacionesLogin = require('../middleware/loginValidations');
 const validacionesRegistro = require('../middleware/registerValidation');
+const { ingresar } = require('../controllers/usersController');
 
 let archivoUsuarios =  JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/users.json')));
 
@@ -28,7 +29,7 @@ const upload= multer({ storage })
 
 //rustas login
 router.get('/login',usersController.login);
-router.post('/login', validacionesLogin);
+router.post('/login', validacionesLogin,usersController,ingresar);
 //rutas register
 router.get('/register',usersController.register);
 router.post('/register', upload.single('avatar'), validacionesRegistro, usersController.create);
