f9705815860d7e3280014b19a9fa0398ecc6f0a4
MateoF01 <mateofernandez380@gmail.com>
2023-03-06 18:39:46 -0300

    Revert "arreglo el recuadro del producto en css"
    This reverts commit 86acc3a2ab0d57a3d1cdf44f5c9404ecc3b1433f.

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 module.exports ={
   "development": {
     "username": "root",
-    "password": "klenex11",
+    "password": "root",
     "database": "structure",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": "3306"
+    "port": "8889"
   },
   "test": {
     "username": "root",
